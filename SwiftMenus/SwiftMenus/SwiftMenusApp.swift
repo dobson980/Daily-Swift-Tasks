0e2724017c2d0b9d210b66fc6448d85e3ca44608
Tom Dobson <Dobson980@gmail.com>
2025-03-18 21:02:17 -0700

    added SwiftMenus Project

---
+++ SwiftMenus/SwiftMenus/SwiftMenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMenusApp.swift
+//  SwiftMenus
+//
+//  Created by Thomas Dobson on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
