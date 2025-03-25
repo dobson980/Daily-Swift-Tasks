9b08d0cb61ad8f3ce01f0763bde7f34c55671cd8
Tom Dobson <Dobson980@gmail.com>
2025-03-24 20:28:11 -0700

    Added SwiftStockApp project

---
+++ SwiftStock/SwiftStock/SwiftStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStockApp.swift
+//  SwiftStock
+//
+//  Created by Thomas Dobson on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
